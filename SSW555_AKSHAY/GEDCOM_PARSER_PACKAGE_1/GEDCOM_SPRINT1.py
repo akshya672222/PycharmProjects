4c2920e1d6c52915c58464b5c118a12fb28e4dc6
Akshay Sunderwani <akshya672222@gmail.com>
2016-09-17 13:03:42 -0400

    check

---
+++ SSW555_AKSHAY/GEDCOM_PARSER_PACKAGE_1/GEDCOM_SPRINT1.py
@@ -0,0 +1,25 @@
+"""
+Gedcom file parser
+Owner: Akshay Sunderwani
+Student: Stevens Institute of Technology, Fall - 2016, Software engineering
+Course: 555 - Agile Methodologies
+File description:
+User Story 13:  Siblings spacing
+User Story Description: Birth dates of siblings should be more than 8 months apart or less than 2 days apart
+Start date: Fri 16 Sept 2016
+"""
+
+import os
+import sys
+
+path = os.getcwd()  # method to fetch working directory path.
+path2 = path + "/GEDCOM_READER_PARSER.py"
+
+sys.path.insert(0, path2)
+
+import GEDCOM_READER_PARSER
+
+
+
+
+
